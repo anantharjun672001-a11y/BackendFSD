5020b2fefdd9ba5da9c9ff20be711d9da9d689b3
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-29 12:43:13 +0530

    authMiddleware created successfully

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = {
+      id: decoded.id,
+      role: decoded.role,
+    };
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+
+export const allowRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+    next();
+  };
+};
