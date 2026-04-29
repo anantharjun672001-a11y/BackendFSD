1c144b94b94f9e10ecaa589ba5f47e5a25524d3b
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-29 12:30:24 +0530

    authRoute connected to index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors'; 
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.use(cors());
 app.use(express.json());
