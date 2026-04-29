2da4e026d37620e7500f93a8a90089a6fb1f3c13
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-29 12:47:29 +0530

    testRoutes connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoutes from "./routes/authRoutes.js";
+import testRoutes from "./routes/testRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/test", testRoutes);
 
 //DB Connect
 mongoose.connect(process.env.MONGODB_URL)
