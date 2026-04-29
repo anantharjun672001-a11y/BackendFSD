7db79976d41f9338a12b4b14bfd71e6f8d0df6ef
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-29 12:22:38 +0530

    base file setuped

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors'; 
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//DB Connect
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
