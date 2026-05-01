d2d0f400f847f2e2650fede32ba3fe2c0764a635
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-01 11:16:29 +0530

    testRoutes updated

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { verifyToken } from "../middleware/authMiddleware.js";
-import { allowRoles } from "../middleware/roleMiddleware.js";
+import { verifyToken,allowRoles } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
