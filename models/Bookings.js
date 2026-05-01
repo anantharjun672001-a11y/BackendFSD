9f6a32336ca2c73b4544ed44b3fd283caa01519f
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-01 11:20:14 +0530

    booking models created

---
+++ models/Bookings.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  service: {
+    type: String,
+  },
+  date: {
+    type: Date,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending", 
+  },
+});
+
+export default mongoose.model("Booking", bookingSchema);
