82fefcf91235586c2bca9152649855b685fd3702
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-02 08:43:40 +0530

    serviceModel created successful

---
+++ models/serviceModels.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const serviceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+});
+
+export default mongoose.model("Service", serviceSchema);
