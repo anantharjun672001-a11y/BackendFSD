502664f859ea2e453e618c9c2955c04acf2c4e17
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-04 19:43:56 +0530

    send mail updated

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,7 +3,9 @@ import nodemailer from "nodemailer";
 export const sendMail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -20,5 +22,6 @@ export const sendMail = async (to, subject, text) => {
     console.log("Mail sent");
   } catch (err) {
     console.log("Mail error:", err);
+    console.log("Mail error:", err.message);
   }
 };
