333e4af7f2599a13c120730b1bf747256b0d359e
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-04 11:08:25 +0530

    nodemailer imported and coded

---
+++ utils/sendMail.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+
+export const sendMail = async (to, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("Mail sent");
+  } catch (err) {
+    console.log("Mail error:", err);
+  }
+};
