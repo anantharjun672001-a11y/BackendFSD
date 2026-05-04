83a4c64ef821c39b0707aecadd21ee7efc650811
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-04 20:04:30 +0530

    code newly updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://frontend-fsd-nu.vercel.app", "http://localhost:5173/"],
+    origin: ["https://frontend-fsd-nu.vercel.app", "https://localhost:5173/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
