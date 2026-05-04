daa20a09977e39c774ac4012e4c57fee000ec6e1
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-04 12:45:13 +0530

    booking modal updated

--- models/Bookings.js
+++ models/Bookings.js
@@ -16,7 +16,7 @@ const bookingSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["pending", "approved", "rejected"],
+    enum: ["pending", "approved", "rejected", "cancelled"],
     default: "pending", 
   },
   paymentStatus: {
